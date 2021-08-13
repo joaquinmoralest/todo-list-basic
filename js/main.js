f1e0b177c5702e344e6191201ddb488e691b4bb5
Joaquin Morales <joaquin.moralestr@gmail.com>
2021-08-13 11:16:21 -0400

    Se escriben las principales funciones que se ejecutaran en la app

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
+//Variables
 const input = document.querySelector('#input');
 const btnEnviar = document.querySelector('#btn-enviar');
 const form = document.querySelector('#form');
 const toDoList = document.querySelector('#todo-list');
 
-eventListeners();
+//Funciones
 
-function eventListeners() {
-    probando();
-}
 
-function probando() {
-    input.addEventListener('click', () => {
-        console.log('probando...')
-    })
-}
